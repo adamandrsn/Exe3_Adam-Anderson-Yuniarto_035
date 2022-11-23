e23f0966725b18a86eb3e7af1d018b63e8e0e045
Adam Anderson <adam.anderson.ft21@mail.umy.ac.id>
2022-11-23 15:48:41 +0700

    membuat method public bool listEmpty

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -34,6 +34,13 @@ namespace Exercise3
             else
                 return (false);/*returns false if the node is not found*/
         }
+        public bool listEmpty()
+        {
+            if (LAST == null)
+                return true;
+            else
+                return false;
+        }
     }
 }
 
